d16e8f8c1758af235c51b9f2beede79514357ea8
MrMaladroit <jamesbeckjr@gmail.com>
2018-12-05 23:33:20 -0500

    Added Player Animation
    Idle Animation
    Running Animation

--- ZigZagClone/Assets/Scripts/PlayerMovementController.cs
+++ ZigZagClone/Assets/Scripts/PlayerMovementController.cs
@@ -5,15 +5,23 @@ public class PlayerMovementController : MonoBehaviour
 {
     private Rigidbody rb;
     private bool isWalkingRight = true;
+    private bool isGameRunning = false;
+    private Animator animationController;
 
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        animationController = GetComponent<Animator>();
     }
 
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            isGameRunning = true;
+            animationController.SetBool("isGameRunning", isGameRunning);
+        }
         if(Input.GetKeyDown(KeyCode.Space))
         {
             TurnCharacter();
@@ -23,7 +31,10 @@ public class PlayerMovementController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        MoveCharacterForward();
+        if(isGameRunning)
+        {
+            MoveCharacterForward();
+        }
     }
 
     private void MoveCharacterForward()
