afe119e3a008846e45926e0754fa15bc13f784cc
MrMaladroit <jamesbeckjr@gmail.com>
2018-12-05 23:27:36 -0500

    Created PlayerMovementController

---
+++ ZigZagClone/Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+    private Transform cameraPosition;
+    private Vector3 offset = new Vector3(2, 3, -2);
+
+    private void Update()
+    {
+        gameObject.transform.position = target.transform.position + offset;
+    }
+
+}
---
+++ ZigZagClone/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private bool isWalkingRight = true;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            TurnCharacter();
+        }
+    }
+
+
+    private void FixedUpdate()
+    {
+        MoveCharacterForward();
+    }
+
+    private void MoveCharacterForward()
+    {
+        rb.transform.position = transform.position + transform.forward * 2 * Time.deltaTime;
+    }
+
+    private void TurnCharacter()
+    {
+        isWalkingRight = !isWalkingRight;
+
+        if (isWalkingRight)
+            transform.rotation = Quaternion.Euler(0, 45, 0);
+        else
+            transform.rotation = Quaternion.Euler(0, -45, 0);
+
+    }
+}
