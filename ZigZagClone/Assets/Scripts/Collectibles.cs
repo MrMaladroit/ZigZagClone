4914f6e7d60de02a24549dd4508f37164f06603a
MrMaladroit <jamesbeckjr@gmail.com>
2018-12-06 11:57:52 -0500

    Created Simple Crystal Particle Effect

---
+++ ZigZagClone/Assets/Scripts/Collectibles.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class Collectibles : MonoBehaviour
+{
+    // If more collectibles are add, extract this into a Crystal class that inherits this class.
+    #region Crystal Collectible Code
+    private ParticleSystem particleSystemInChild;
+    private MeshRenderer meshRendererComponent; 
+
+
+    private void Awake()
+    {
+        particleSystemInChild = GetComponentInChildren<ParticleSystem>();
+        meshRendererComponent = GetComponent<MeshRenderer>();
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag != "Player")
+        {
+            return;
+        }
+
+        Debug.Log("OnTriggerEnter Triggered By Player");
+
+        particleSystemInChild.Play();
+        meshRendererComponent.enabled = false;
+        
+        Destroy(gameObject, 2);
+    }
+    #endregion
+
+}
